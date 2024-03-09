cff7139ed7746a87c0487ad40c6630ec9c413702
SurjeetMandal <surjeetmandal698@gmail.com>
2024-03-09 11:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    arr.sort((a, b) => a - b);
+
+    let cost = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        const mergeCost = arr[i - 1] + arr[i];
+        cost += mergeCost;
+
+        arr[i] = mergeCost;
+    }
+
+    return cost;
+}
+	
+const arr = [4, 3, 2, 6];
+console.log(mincost(arr));
 }
 
 module.exports=mincost;
